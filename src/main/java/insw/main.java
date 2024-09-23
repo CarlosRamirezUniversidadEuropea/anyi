a66d9de358f314b9907bbf756077272ef1c4885b
咕咕嘎嘎 <linanyi@xiaolinMacBook-Pro.local>
2024-09-23 20:55:59 +0200

    first commit

---
+++ src/main/java/insw/main.java
@@ -0,0 +1,26 @@
+
+package insw;
+
+
+public class main {
+
+
+ public static void main(String args[]) {
+
+
+ Calculator calculator = new Calculator();
+
+
+ System.out.println(calculator.multiply(2, 4));
+
+
+ System.out.println("Hola Mundo");
+
+
+ }
+
+
+ 
+
+
+}
---
+++ src/test/java/BuscadorTestCase.java
@@ -0,0 +1,25 @@
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.Test;
+import insw.Buscador;
+import java.util.Arrays;
+import java.util.List;
+ 
+class BuscadorTestCase {
+	Buscador buscador;
+	List<String> lista;
+	
+	void setUp(){
+		buscador = new Buscador();
+		  
+    }
+
+	
+	
+
+}
+
